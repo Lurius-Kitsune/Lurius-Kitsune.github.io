30e51b59ae331598c843e2bed2670e6aa204b30d
Lurius-Kitsune <LucasFox@outlook.fr>
2023-08-28 16:11:03 +0200

    🔧 Optimised footer + Link image on Nolark

--- index.php
+++ index.php
@@ -65,65 +65,8 @@
         </div>
     </div> <!-- Emplacement pour le contenu principal de la page -->
     <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <h2>Étudiant au lycée Bonaparte</h2>
-                    <p>J'ai choisi de suivre le BTS SIO au lycée Bonaparte dû aux échos d'une formation en «béton»,
-                        car je souhaitais pouvoir suivre cette formation dans un lieu où je pourrais être sur de la
-                        qualité.
-                        Dans ce lycée, la formation ce veux d'être professionnalisante et ce veux donc d'être sélective
-                        et
-                        avec un taux de réussite élevé.
-                    </p>
-                    <p><a class="btn btn-secondary" href="https://bts-sio.lyc-bonaparte.fr" target="_blank"
-                            role="button">En savoir
-                            plus »</a>
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Le BTS SIO</h2>
-                    <p>Passionnée d'informatique depuis mon plus jeune âge, j'aime apprendre à faire
-                        de nouvelles choses avec mon ordinateur, mon smartphone ou ma tablette. Après mon bac, le BTS
-                        SIO s'est présenté comme une évidence. Avec une plus grande partie des cours étant de
-                        l'informatique, et
-                        le reste étant de l'Économie, management et droit de l'informatique, Anglais (Une matiére que
-                        j'adore !), culture
-                        générale et Maths, sans oublier les cours de cybersécurité présent afin de nous formet au danger
-                        et au risque de l'informatique
-                        . Ce tout me permet encore plus déterminer à faire cette formation afin d'atteindre mon but !
-                    </p>
-                    <p><a class="btn btn-secondary" href="https://fr.wikipedia.org/wiki/Brevet_de_tec
-                                  hnicien_supérieur_-_Services_informatiques_aux_organisations" target="_blank"
-                            role="button">En savoir plus »</a>
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Aprés ce BTS SIO ? </h2>
-                    <p>Tout d'abord je me suis axé dans l'option SLAM : "Solutions logicielles et Applications Métiers",
-                        étant donné que je souhaite m'approcher du métier de développeur en maitrisant les bases du
-                        développement
-                        tel que la gestion de versions ou l'orienté objet, ainsi que des languages de programmation tels
-                        que Java,
-                        C#, C++ et autres... Par la suite, j'aimerais rejoindre l'école Objectif 3D (vois Bouton en
-                        savoir plus)
-                        situé à MONTPELLIER dans le but de poursuivre mes études dedans.
-                    </p>
-                    <p><a class="btn btn-secondary" href="https://www.objectif3d.com" target="_blank" role="button">En
-                            savoir plus »</a>
-                    </p>
-                </div>
-            </div>
-            <hr>
-            <p class="text-center" id="copyright">
-                Le fond est une image de 
-                <a href="https://fr.freepik.com/vecteurs-libre/fond-technologie-hud-abstrait-bleu_5197348.htm#query=informatique&position=4&from_view=keyword">
-                    pikisuperstar</a> 
-                sur Freepik
-            </p>
-            <p class="text-center" id="copyright">© Lucas Bruel 2022-2023</p>
-        </div>
-    </footer>
+        <?php require_once __DIR__ . '/include/Defaultfooter.php'; ?>
+    </footer> 
 </body>
 
 </html>
--- pages/realisation/Nolark.php
+++ pages/realisation/Nolark.php
@@ -33,7 +33,7 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card mb-4 shadow-sm border-sm border-red">
-                            <img class="card-img-top" src="../../images/Nolark/acceuil_nolark.png">
+                            <img class="card-img-top" src="/images/Nolark/acceuil_nolark.png">
                             <div class="card-body">
                                 <p class="card-text">
                                     La page d'acceuil du site Nolark
@@ -41,35 +41,35 @@
                                 <br>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <button type="button" class="btn btn-sm btn-outline-secondary"><a
-                                            href="../../images/Nolark/acceuil_nolark.png" target="_blank">Voir</a></button>
+                                            href="/images/Nolark/acceuil_nolark.png" target="_blank">Voir</a></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card mb-4 shadow-sm border-sm border-red">
-                            <img class="card-img-top" src="../../images/Nolark/nolark_html.png">
+                            <img class="card-img-top" src="/images/Nolark/nolark_html.png">
                             <div class="card-body">
                                 <p class="card-text">
                                     Le code HTML de la page d'acceuil
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <button type="button" class="btn btn-sm btn-outline-secondary"><a
-                                            href="../../resources/Nolark_index.txt" target="_blank">Voir</a></button>
+                                            href="/resources/Nolark_index.txt" target="_blank">Voir</a></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card mb-4 shadow-sm border-sm border-red">
-                            <img class="card-img-top" src="../../images/Nolark/nolark_css.png">
+                            <img class="card-img-top" src="/images/Nolark/nolark_css.png">
                             <div class="card-body">
                                 <p class="card-text">
                                     Exemple de code CSS fait pour la page
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <button type="button" class="btn btn-sm btn-outline-secondary"><a
-                                            href="../../resources/Nolark_css.txt">Voir</a></button>
+                                            href="/resources/Nolark_css.txt">Voir</a></button>
                                 </div>
                             </div>
                         </div>
@@ -86,65 +86,7 @@
         </div>
     </div> <!-- Emplacement pour le contenu principal de la page -->
     <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <h2>Étudiant au lycée Bonaparte</h2>
-                    <p>J'ai choisi de suivre le BTS SIO au lycée Bonaparte dû aux échos d'une formation en «béton»,
-                        car je souhaitais pouvoir suivre cette formation dans un lieu où je pourrais être sur de la
-                        qualité.
-                        Dans ce lycée, la formation ce veux d'être professionnalisante et ce veux donc d'être sélective
-                        et
-                        avec un taux de réussite élevé.
-                    </p>
-                    <p><a class="btn btn-secondary" href="https://bts-sio.lyc-bonaparte.fr" target="_blank"
-                            role="button">En savoir
-                            plus »</a>
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Le BTS SIO</h2>
-                    <p>Passionnée d'informatique depuis mon plus jeune âge, j'aime apprendre à faire
-                        de nouvelles choses avec mon ordinateur, mon smartphone ou ma tablette. Après mon bac, le BTS
-                        SIO s'est présenté comme une évidence. Avec une plus grande partie des cours étant de
-                        l'informatique, et
-                        le reste étant de l'Économie, management et droit de l'informatique, Anglais (Une matiére que
-                        j'adore !), culture
-                        générale et Maths, sans oublier les cours de cybersécurité présent afin de nous formet au danger
-                        et au risque de l'informatique
-                        . Ce tout me permet encore plus déterminer à faire cette formation afin d'atteindre mon but !
-                    </p>
-                    <p><a class="btn btn-secondary" href="https://fr.wikipedia.org/wiki/Brevet_de_tec
-                                  hnicien_supérieur_-_Services_informatiques_aux_organisations" target="_blank"
-                            role="button">En savoir plus »</a>
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Aprés ce BTS SIO ? </h2>
-                    <p>Tout d'abord je me suis axé dans l'option SLAM : "Solutions logicielles et Applications Métiers",
-                        étant donné que je souhaite m'approcher du métier de développeur en maitrisant les bases du
-                        développement
-                        tel que la gestion de versions ou l'orienté objet, ainsi que des languages de programmation tels
-                        que Java,
-                        C#, C++ et autres... Par la suite, j'aimerais rejoindre l'école Objectif 3D (vois Bouton en
-                        savoir plus)
-                        situé à MONTPELLIER dans le but de poursuivre mes études dedans.
-                    </p>
-                    <p><a class="btn btn-secondary" href="https://www.objectif3d.com" target="_blank" role="button">En
-                            savoir plus »</a>
-                    </p>
-                </div>
-            </div>
-            <hr>
-            <p class="text-center" id="copyright">
-                Le fond est une image de
-                <a
-                    href="https://fr.freepik.com/vecteurs-libre/fond-technologie-hud-abstrait-bleu_5197348.htm#query=informatique&position=4&from_view=keyword">
-                    pikisuperstar</a>
-                sur Freepik
-            </p>
-            <p class="text-center" id="copyright">© Lucas Bruel 2022</p>
-        </div>
+        <?php require_once __DIR__ . '/../../include/Defaultfooter.php'; ?>
     </footer>
 </body>
 
