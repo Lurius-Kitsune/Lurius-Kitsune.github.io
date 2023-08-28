b4eb7b7f06f1a2923918c778dadbf754897c6eba
Lurius-Kitsune <LucasFox@outlook.fr>
2023-08-28 17:52:52 +0200

    🗨️Correction de la page Nolark
    Fixes #29

--- pages/realisation/Nolark.php
+++ pages/realisation/Nolark.php
@@ -23,8 +23,8 @@
                     <span class="underline font-weight-bold">Description:</span>
                     <p class="font-italic">
                         Ce site vise à avertir des dangers présents sur la route ainsi que l’importance du port du casque
-                        pour les conducteurs de deux roues. On y trouve une section visant à sensibiliser les usagers de
-                        la route, une section d’achat en ligne avec une large gamme de produits disponible mais aussi
+                        pour les conducteurs de deux-roues. On y trouve une section visant à sensibiliser les usagers de
+                        la route, une section d’achat en ligne avec une large gamme de produits disponible, mais aussi
                         une page de contact.
                     </p>
                 </li>
@@ -36,7 +36,7 @@
                             <img class="card-img-top" src="/images/Nolark/acceuil_nolark.png">
                             <div class="card-body">
                                 <p class="card-text">
-                                    La page d'acceuil du site Nolark
+                                    La page d'acceuil du site Nolark.
                                 </p>
                                 <br>
                                 <div class="d-flex justify-content-between align-items-center">
@@ -51,7 +51,7 @@
                             <img class="card-img-top" src="/images/Nolark/nolark_html.png">
                             <div class="card-body">
                                 <p class="card-text">
-                                    Le code HTML de la page d'acceuil
+                                    Le code HTML de la page d'acceuil.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <button type="button" class="btn btn-sm btn-outline-secondary"><a
@@ -65,7 +65,7 @@
                             <img class="card-img-top" src="/images/Nolark/nolark_css.png">
                             <div class="card-body">
                                 <p class="card-text">
-                                    Exemple de code CSS fait pour la page
+                                    Exemple de code CSS fait pour la page.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <button type="button" class="btn btn-sm btn-outline-secondary"><a
@@ -77,7 +77,7 @@
                 </div>
                 <p class="font-italic">
                     Le projet Nolark est un site fictif développé pendant le début de la 1 ére année de BTS SIO par
-                    une série de TP réalisé par les étudiants afin d' apprendre les bases du développement en HTML,
+                    une série de TP réalisé par les étudiants afin d'apprendre les bases du développement en HTML,
                     en CSS, en JS ainsi qu’une introduction sur le PHP. 
                 </p>
                 <a href="https://github.com/Lurius-Kitsune/Tp-Nolark" target="_blank"><button class="btn btn-secondary" style="width: 135px"><span class="fa fa-github "></span> Code source</button></a>
