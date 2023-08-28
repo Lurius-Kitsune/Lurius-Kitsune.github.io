9c2d7ce042f9b29175246934bbcbaa55e5292f28
Lurius-Kitsune <LucasFox@outlook.fr>
2023-08-28 16:26:39 +0200

    🗨️Correction de la page index
    Fixes #29

--- index.php
+++ index.php
@@ -14,24 +14,24 @@
             <img src="images/identiter.jpg" alt="Mon visage" class="img-thumbnail mr-2 float-left "
                 style="height: 310px;">
             <p class="ml-1">
-                Je m'appelle Lucas Bruel, j'ai 19 ans et depuis tout petit j'ai été baigné par l'informatique et les
+                Je m'appelle Lucas Bruel, j'ai 19 ans et depuis tout petit, j'ai été baigné par l'informatique et les
                 nouvelles technologies.
             </p>
             <p class="ml-1">
                 J'ai donc poursuivis cette envie et me suis inscrit en B.T.S SIO après mon baccalauréat professionnel
                 Systèmes Numériques et Numériques
-                en option Réseaux Informatique et systèmes communiquant m'ayant donnés un bagage dans l'informatique plu
+                en option Réseaux Informatique et systèmes communicant m'ayant donnés un bagage dans l'informatique plus
                 précisément en réseaux,
-                pendant mon BAC j'ai eu l'ambition de faire de ma passion, mon métier.
+                et donc, pendant mon BAC, j'ai eu l'ambition de faire de ma passion, mon métier.
             </p>
             <p class="ml-1">
-                Ces pous cela que je me suis axé dans le développement, fut mon choix en rejoignant l'option SLAM de
-                ceux BTS, cela me permettra
-                d'avoir les bagage requis permettant d'atteindre mon projet professionel.
+                Ces pour cela que je me suis axé dans le développement, fut mon choix en rejoignant l'option SLAM de
+                ce BTS, cela me permettra
+                d'avoir les bagages requis permettant d'atteindre mon projet professionnel.
             </p>
             <div class="d-flex flex-column">
                 <p class="ml-1">
-                    Après avoir effectué plusieurs formation en milieu professionnel, principalement dans le domaine du
+                    Après avoir effectué plusieurs formations en milieu professionnel, principalement dans le domaine du
                     développement informatique,
                     je souhaite poursuivre mon expérience professionnelle dans ce domaine afin de devenir "game dev"
                 </p>
