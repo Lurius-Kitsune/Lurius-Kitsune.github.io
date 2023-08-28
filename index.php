ece507ae1a480c6885048a832068a2f7e05f9e16
Lurius-Kitsune <LucasFox@outlook.fr>
2023-08-28 16:05:44 +0200

    🚅 Conversion html to php

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <?php require_once __DIR__ . '/include/head.inc.html'; ?>
+</head>
+
+<body>
+    <?php require_once __DIR__ . '/include/nav.inc.php'; ?>
+
+    <div class="jumbotron img-1">
+        <div class="box-lg rounded-top border-sm border-blue bg-white">
+            <h1 class="underline">LUCAS BRUEL, FUTUR GAME DÉVELOPPEUR</h1>
+            <img src="images/identiter.jpg" alt="Mon visage" class="img-thumbnail mr-2 float-left "
+                style="height: 310px;">
+            <p class="ml-1">
+                Je m'appelle Lucas Bruel, j'ai 19 ans et depuis tout petit j'ai été baigné par l'informatique et les
+                nouvelles technologies.
+            </p>
+            <p class="ml-1">
+                J'ai donc poursuivis cette envie et me suis inscrit en B.T.S SIO après mon baccalauréat professionnel
+                Systèmes Numériques et Numériques
+                en option Réseaux Informatique et systèmes communiquant m'ayant donnés un bagage dans l'informatique plu
+                précisément en réseaux,
+                pendant mon BAC j'ai eu l'ambition de faire de ma passion, mon métier.
+            </p>
+            <p class="ml-1">
+                Ces pous cela que je me suis axé dans le développement, fut mon choix en rejoignant l'option SLAM de
+                ceux BTS, cela me permettra
+                d'avoir les bagage requis permettant d'atteindre mon projet professionel.
+            </p>
+            <div class="d-flex flex-column">
+                <p class="ml-1">
+                    Après avoir effectué plusieurs formation en milieu professionnel, principalement dans le domaine du
+                    développement informatique,
+                    je souhaite poursuivre mon expérience professionnelle dans ce domaine afin de devenir "game dev"
+                </p>
+                <a href="/pages/cv.php"><button class="btn btn-primary" style="width: 135px" >En savoir plus »</button></a>
+            </div>
+            <div>
+                <h4 class="underline">Contenue du portfolio :</h4>
+                <p> A travers ceux portfolio vous allez pouvoir naviguer à travers plusieur pages qui vous donneront
+                    les information suivant :
+                </p>
+                <ul>
+                    <li>Une présentation du lycée où j'ai passée mon BTS SIO</li>
+                    <li>Mon Curriculum Vitae.</li>
+                    <li>Mes réalisation en Automie, en Entreprise et pendant mes études.</li>
+                    <li>Une catégorie en lien avec l'épreuve E5. (sont remplisage se fera au cours de la 2nd année de
+                        BTS SIO)</li>
+                </ul>
+            </div>
+            <div>
+                <h4 class="underline">Le but de ceux portfolio :</h4>
+                <p>
+                    Ce portfolio me permet de montrer mes compétences ainsi que mes capacité afin de pouvoir me
+                    démarquer lors d'un recrutement. Il répertorira
+                    mon profil ainsi que toute documentation, expériences professionel et stages accumulé au fil du
+                    temps.
+                </p>
+                <p>
+                    Ce portfolio fait aussi partie de l'épreuve E5 du BTS SIO et sera donc jugé et noté par un jury.
+                </p>
+            </div>
+        </div>
+    </div> <!-- Emplacement pour le contenu principal de la page -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h2>Étudiant au lycée Bonaparte</h2>
+                    <p>J'ai choisi de suivre le BTS SIO au lycée Bonaparte dû aux échos d'une formation en «béton»,
+                        car je souhaitais pouvoir suivre cette formation dans un lieu où je pourrais être sur de la
+                        qualité.
+                        Dans ce lycée, la formation ce veux d'être professionnalisante et ce veux donc d'être sélective
+                        et
+                        avec un taux de réussite élevé.
+                    </p>
+                    <p><a class="btn btn-secondary" href="https://bts-sio.lyc-bonaparte.fr" target="_blank"
+                            role="button">En savoir
+                            plus »</a>
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Le BTS SIO</h2>
+                    <p>Passionnée d'informatique depuis mon plus jeune âge, j'aime apprendre à faire
+                        de nouvelles choses avec mon ordinateur, mon smartphone ou ma tablette. Après mon bac, le BTS
+                        SIO s'est présenté comme une évidence. Avec une plus grande partie des cours étant de
+                        l'informatique, et
+                        le reste étant de l'Économie, management et droit de l'informatique, Anglais (Une matiére que
+                        j'adore !), culture
+                        générale et Maths, sans oublier les cours de cybersécurité présent afin de nous formet au danger
+                        et au risque de l'informatique
+                        . Ce tout me permet encore plus déterminer à faire cette formation afin d'atteindre mon but !
+                    </p>
+                    <p><a class="btn btn-secondary" href="https://fr.wikipedia.org/wiki/Brevet_de_tec
+                                  hnicien_supérieur_-_Services_informatiques_aux_organisations" target="_blank"
+                            role="button">En savoir plus »</a>
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Aprés ce BTS SIO ? </h2>
+                    <p>Tout d'abord je me suis axé dans l'option SLAM : "Solutions logicielles et Applications Métiers",
+                        étant donné que je souhaite m'approcher du métier de développeur en maitrisant les bases du
+                        développement
+                        tel que la gestion de versions ou l'orienté objet, ainsi que des languages de programmation tels
+                        que Java,
+                        C#, C++ et autres... Par la suite, j'aimerais rejoindre l'école Objectif 3D (vois Bouton en
+                        savoir plus)
+                        situé à MONTPELLIER dans le but de poursuivre mes études dedans.
+                    </p>
+                    <p><a class="btn btn-secondary" href="https://www.objectif3d.com" target="_blank" role="button">En
+                            savoir plus »</a>
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <p class="text-center" id="copyright">
+                Le fond est une image de 
+                <a href="https://fr.freepik.com/vecteurs-libre/fond-technologie-hud-abstrait-bleu_5197348.htm#query=informatique&position=4&from_view=keyword">
+                    pikisuperstar</a> 
+                sur Freepik
+            </p>
+            <p class="text-center" id="copyright">© Lucas Bruel 2022-2023</p>
+        </div>
+    </footer>
+</body>
+
+</html>
--- pages/cv.php
+++ pages/cv.php
@@ -2,139 +2,12 @@
 <html lang="fr">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Portfolio de Lucas Bruel - BTS SIO SLAM">
-    <meta name="author" content="Lucas Bruel">
-    <link rel="icon" href="../favicon.ico">
-    <link href="../css/cv.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
-    <title>Portfolio de Lucas Bruel</title>
+    <?php require_once __DIR__ . '/../include/head.inc.html'; ?>
+    <link href="/css/cv.css" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-primary fixed-top navbar-expand-md">
-        <div class="container">
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar"
-                aria-controls="navbar">
-                <span class="sr-only">Navigation raccourcie</span>☰
-            </button>
-            <a class="navbar-brand" href="#">Mon portfolio</a>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item">
-                        <a href="../index.html" class="nav-link">
-                            <span class="fa fa-home"></span> Accueil
-                        </a>
-                    </li>
-                    <li class="active nav-item">
-                        <a href="cv.html" class="nav-link">
-                            <span class="fa fa-id-badge"></span> CV
-                        </a>
-                    </li>
-                    <li class="dropdown nav-item">
-                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button">
-                            <span class="fa fa-list-alt"></span> Réalisations <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li role="presentation" class="dropdown-header dropdown-item">
-                                En autonomie</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Bot Discord Eski</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Texturisation modèle 3D</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Configuration de modèle 3D</a>
-                            </li>
-                            <li role="presentation" class="dropdown-header dropdown-item">
-                                Pendant mes étude</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="realisation/Nolark.html">Nolark</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Mon portfolio</a>
-                            </li>
-                            <li role="presentation" class="dropdown-header dropdown-item">
-                                En entreprise</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Projet de 1ère année</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Projet de 2ème année</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown nav-item">
-                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button">
-                            <span class="fa fa-graduation-cap"></span>
-                            Jury E5 BTS SIO
-                            <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li class="dropdown-header dropdown-item" role="presentation">
-                                Documentations contexte</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Présentation</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Application</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Missions à réaliser</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Annexes</a>
-                            </li>
-                            <li role="presentation" class="dropdown-divider"></li>
-                            <li role="presentation" class="dropdown-header dropdown-item">
-                                Fiches d'examen</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Situation professionnelle n°1</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Situation professionnelle n°2</a>
-                            </li>
-                            <li role="presentation" class="dropdown-divider"></li>
-                            <li role="presentation" class="dropdown-header dropdown-item">
-                                Démonstrations</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Démo SP n°1</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Démo SP n°2</a>
-                            </li>
-                            <li role="presentation" class="dropdown-divider"></li>
-                            <li role="presentation" class="dropdown-header dropdown-item">
-                                Codes sources</li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Sources SP n°1</a>
-                            </li>
-                            <li role="presentation" class="dropdown-item">
-                                <a role="menuitem" href="#">Sources SP n°2</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-        crossorigin="anonymous"></script>
+    <?php require_once __DIR__ . '/../include/nav.inc.php'; ?>
 
     <div class="jumbotron img-1">
         <div class="container">
@@ -155,7 +28,7 @@
                         <p>
                             Ayant actuellement 19 ans, je souhaite faire de ma passion pour le développement
                             mon métier, ces pour cela que je fait actuellement un BTS Services Informatiques aux Organisations
-                            afin de progresser dans le domaines et y acquérir les compétences requise &nbsp;!
+                            afin de progresser dans le domaines et y acquérir les compétences requise&nbsp;!
                         </p>
                     </div>
                     <div class="rubrique clearfix">
