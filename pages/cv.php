026c4795ec5a2d052c27ab6788aa014c490c6ae1
Lurius-Kitsune <LucasFox@outlook.fr>
2023-08-30 12:38:34 +0200

    🆕Added Evoliz Experience

--- pages/cv.php
+++ pages/cv.php
@@ -71,6 +71,38 @@
                         <h2 class="underline"><span class="fa fa-briefcase"></span> Expériences professionnelles</h2>
                         <div class="experience clearfix">
                             <div class="col-3">
+                                <div class="lieu">Evoliz</div>
+                                <div class="date">2023 (8 sem.)</div>
+                            </div>
+                            <div class="col-9">
+                                <div class="profession">Developpeur Web</div>
+                                <div class="details">
+                                    <ul>
+                                        <li>Ajout et amélioration de fonctionnalité de la solution Evoliz.</li>
+                                        <li>Maintenance et correction de la solution Evoliz.</li>
+                                        <li>Découverte du framework PHP Laravel et des methodes de développement avec pomodoro.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="experience clearfix">
+                            <div class="col-3">
+                                <div class="lieu">Linexos</div>
+                                <div class="date">2021 (6 sem.)</div>
+                            </div>
+                            <div class="col-9">
+                                <div class="profession">Developpeur Python</div>
+                                <div class="details">
+                                    <ul>
+                                        <li>Développement d’une API ainsi qu’une REST API en Python.</li>
+                                        <li>Travaille sur un début de projet d’un client de l’entreprise.</li>
+                                        <li>Découverte de python en autodidacte.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="experience clearfix">
+                            <div class="col-3">
                                 <div class="lieu">Linexos</div>
                                 <div class="date">2021 (6 sem.)</div>
                             </div>
