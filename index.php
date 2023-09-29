2f53078be3f2e246c0069d1b4b6a6df6e142665a
Lurius-Kitsune <LucasFox@outlook.fr>
2023-09-29 20:50:44 +0200

    🔧 Update présentation texte

--- index.php
+++ index.php
@@ -7,30 +7,23 @@ ob_start();
 <script src="/js/home.js" crossorigin="anonymous"></script>
 <div class="p-5 bg-body-tertiary img-1">
     <div class="box-lg rounded-top border-sm border-red bg-white">
-        <h1 class="underline">LUCAS BRUEL, FUTUR GAME DÉVELOPPEUR</h1>
-        <img src="images/identiter.jpg" alt="Mon visage" class="img-thumbnail mr-2 float-left " style="height: 310px;">
+        <img src="images/identiter.jpg" alt="Mon visage" class="img-thumbnail m-auto mb-1 d-block " style="height: 250px;">
         <p class="ml-1">
-            Je m'appelle Lucas Bruel, j'ai 19 ans et depuis tout petit, j'ai été baigné par l'informatique et les
-            nouvelles technologies.
+            Je m'appelle Lucas Bruel, j'ai 19 ans et, depuis tout petit, j'ai été baigné par l'informatique et les
+            nouvelles technologies avec pour rêves d'être développeur dans les jeux vidéo.
         </p>
         <p class="ml-1">
-            J'ai donc poursuivis cette envie et me suis inscrit en B.T.S SIO après mon baccalauréat professionnel
-            Systèmes Numériques et Numériques
-            en option Réseaux Informatique et systèmes communicant m'ayant donnés un bagage dans l'informatique plus
-            précisément en réseaux,
-            et donc, pendant mon BAC, j'ai eu l'ambition de faire de ma passion, mon métier.
+            J'ai donc commencé par un baccalauréat professionnel Systèmes Numériques, en option Réseaux Informatique
+            et systèmes communicant. Ceci m'ayant donné un bagage dans l'informatique plus précisément en réseaux, et donc, pendant mon BAC,
+            j'ai pu découvrir la programmation et j'ai accroché, c'est pour cela que j'ai eu l'ambition de faire de mes 2 passions, mon métier.
         </p>
         <p class="ml-1">
-            Ces pour cela que je me suis axé dans le développement, fut mon choix en rejoi
-            gnant l'option SLAM de
-            ce BTS, cela me permettra
-            d'avoir les bagages requis permettant d'atteindre mon projet professionnel.
+            J'ai donc décider de rejoindre le BTS SIO avec l'option SLAM qui fut mon choix en rejoignant l'option SLAM de
+            ce BTS. J'ai fait ce choix afin d'avoir les bagages requis et solide me permettant d'atteindre mon projet professionnel.
         </p>
         <div class="d-flex flex-column">
             <p class="ml-1">
-                Après avoir effectué plusieurs formations en milieu professionnel, principalement dans le domaine du
-                développement informatique,
-                je souhaite poursuivre mon expérience professionnelle dans ce domaine afin de devenir "game dev"
+                Je ne sais pas encore ou cette aventure professionnelle m'amènera, mais je ferais tout afin d'atteindre mon rêve !
             </p>
         </div>
 
