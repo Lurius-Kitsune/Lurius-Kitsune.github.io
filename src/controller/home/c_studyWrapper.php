5704e4cd2bb8a5795988f65366eb6c084bb6ddb1
Lurius-Kitsune <LucasFox@outlook.fr>
2023-10-19 19:52:19 +0200

    🔧 Optimised by removing get_defined_vars on studyWrapper

--- src/controller/home/c_studyWrapper.php
+++ src/controller/home/c_studyWrapper.php
@@ -2,49 +2,53 @@
 
 use Luriusfox\MyPackage\Tools\Pager;
 
-?>
-
-<div id="home-study-content">
-    <?php
-    // BTS SIO
-    $studyId = "btssio";
-    $studyName = "BTS SIO option SLAM (en cours...)";
-    $studyYearsPeriod = "2022&nbsp;-&nbsp;2024";
-    $studyState = 'Brevet de Technicien Supérieur "Services Informatiques aux
+// BTS SIO
+$studyTable = [];
+$studyTable['btssio'] = [
+    'name' => "BTS SIO option SLAM (en cours...)",
+    'yearsPeriod' => "2022&nbsp;-&nbsp;2024",
+    'state' => '<span class="badge text-bg-info">EN COURS</span> Brevet de Technicien Supérieur "Services Informatiques aux
                         Organisations", option "Solutions Logicielles et Applications
-                        Métiers"';
-    $studyDescription = ["Diplôme d'État de niveau Bac+2."];
-    $studyDescription[] .= "Obtention de connaissance général et approfondie en developpement informatique.";
-    $studyDescription[] .= "Consolidation sur le tronc commun des connaissances en réseaux et en systèmes.";
-    $linkWebsite = "https://bts-sio.lyc-bonaparte.fr";
-    Pager::renderPage(VIEW . '/home/v_studyTab.php', get_defined_vars());
-    ?>
+                        Métiers"',
+    'description' => [
+        "Diplôme d'État de niveau Bac+2.",
+        "Obtention de connaissance général et approfondie en developpement informatique.",
+        "Consolidation sur le tronc commun des connaissances en réseaux et en systèmes.",
+    ],
+    'linkWebsite' => "https://bts-sio.lyc-bonaparte.fr"
+];
 
-    <?php
-    // BTS SIO
-    $studyId = "bacprosn";
-    $studyName = "Bac Pro S.N Option R.I.S.C";
-    $studyYearsPeriod = "2017&nbsp;-&nbsp;2020";
-    $studyState = '[Mention Trés Bien] Obtention du Baccalauréat professionnel "Systèmes Numérique" option "Réseaux
-                        Informatique et Systèmes Communiquant"';
+// BAC PRO SN
+$studyTable['bacprosn'] = [
+    'name' => "Bac Pro S.N Option R.I.S.C",
+    'yearsPeriod' => "2017&nbsp;-&nbsp;2020",
+    'state' => '<span class="badge text-bg-info">Mention trés bien</span> Baccalauréat professionnel "Systèmes Numérique" option "Réseaux
+                        Informatique et Systèmes Communiquant"',
+    'description' => [
+        "Diplôme d'État de niveau Bac.",
+        "Administration de serveur et de réseau informatique.",
+        "Obtention de connaissances en réseaux.",
+        "Supervision et gestion de projet.",
+        "Création Mini Eco-Entreprise en premiére année."
+    ],
+    'linkWebsite' => "https://lyceebranly.fr"
+];
 
-    $studyDescription = ["Diplôme d'État de niveau Bac."];
-    $studyDescription[] .= "Administration de serveur et de réseau informatique.";
-    $studyDescription[] .= "Obtention de connaissances en réseaux.";
-    $studyDescription[] .= "Supervision et gestion de projet.";
-    $studyDescription[] .= "Création Mini Eco-Entreprise en premiére année.";
-    $linkWebsite = "https://lyceebranly.fr";
-    Pager::renderPage(VIEW . '/home/v_studyTab.php', get_defined_vars());
-    ?>
 
-    <?php
-    // BTS SIO
-    $studyId = "dnb";
-    $studyName = "Diplôme Nationale du Brevet";
-    $studyYearsPeriod = "2018";
-    $studyState = '[Mention Assez Bien] Obtenu';
-    $studyDescription = null;
-    $linkWebsite = null;
-    Pager::renderPage(VIEW . '/home/v_studyTab.php', get_defined_vars());
-    ?>
-</div>
+$studyTable['dnb'] = [
+    'name' => "Diplôme Nationale du Brevet",
+    'yearsPeriod' => "2018",
+    'state' => '<span class="badge text-bg-info">Mention assez bien</span>',
+    'description' => null,
+    'linkWebsite' => null
+];
+foreach ($studyTable as $studyKey => $studyValue) {
+    Pager::renderPage(VIEW . '/home/v_studyTab.php', [
+        'studyId' => $studyKey,
+        'studyName' => $studyValue['name'],
+        'studyYearsPeriod' => $studyValue['yearsPeriod'],
+        'studyState' => $studyValue['state'],
+        'studyDescription' => $studyValue['description'],
+        'linkWebsite' => $studyValue['linkWebsite']
+    ]);
+}
--- src/view/home/v_studyTab.php
+++ src/view/home/v_studyTab.php
@@ -9,31 +9,31 @@
  * @var string $linkWebsite
  */
 ?>
-
-<div class="cardbox cardbox-hoverable"  data-aos="fade-up" data-aos-easing="ease-out-cubic">
-    <div class="row" id="<?= $studyId; ?>">
-        <i class="col-lg-2 fa fa-duotone fa-school" style="font-size: 100px;"></i>
-        <div class="col-lg">
-            <h3><?= $studyName; ?></h3>
-            <h5><?= $studyYearsPeriod; ?></h5>
-            <h6><i class="fa fa-duotone fa-graduation-cap"></i> <?= $studyState; ?></h6>
+<div id="home-study-content">
+    <div class="cardbox cardbox-hoverable" data-aos="fade-up" data-aos-easing="ease-out-cubic">
+        <div class="row" id="<?= $studyId; ?>">
+            <i class="col-lg-2 fa fa-duotone fa-school" style="font-size: 100px;"></i>
+            <div class="col-lg">
+                <h3><?= $studyName; ?></h3>
+                <h5><?= $studyYearsPeriod; ?></h5>
+                <h6><i class="fa fa-duotone fa-graduation-cap"></i> <?= $studyState; ?></h6>
+            </div>
         </div>
-    </div>
-    <?php if ($studyDescription !== null) {
-        echo "<hr>";
-    }
-    ?>
-    <div class="row">
-        <div class="col-lg-10 offset-lg-2">
-            <?php
-            if (is_array($studyDescription) && !empty($studyDescription)) {
-                foreach ($studyDescription as $description) {
-                    echo "<li>$description</li>";
+        <?php if ($studyDescription !== null) {
+            echo "<hr>";
+        }
+        ?>
+        <div class="row">
+            <div class="col-lg-10 offset-lg-2">
+                <?php
+                if (is_array($studyDescription) && !empty($studyDescription)) {
+                    foreach ($studyDescription as $description) {
+                        echo "<li>$description</li>";
+                    }
                 }
-            }
 
-            if ($linkWebsite !== null) {
-                echo "<a href='$linkWebsite' target='_blank'>
+                if ($linkWebsite !== null) {
+                    echo "<a href='$linkWebsite' target='_blank'>
                         <button type='button' class='custom-btn btn-3'>
                             <span>
                                 <i class='fa fa-duotone fa-globe '></i> 
@@ -41,8 +41,9 @@
                             </span>
                         </button>
                     </a>";
-            }
-            ?>
+                }
+                ?>
+            </div>
         </div>
     </div>
 </div>
