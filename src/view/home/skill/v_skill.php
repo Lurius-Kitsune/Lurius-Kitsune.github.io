2a15d14c3242bdf565bf15fbe121430b5f0c7c26
Lurius-Kitsune <LucasFox@outlook.fr>
2023-11-11 16:18:34 +0100

    Fix width issue and update skill tab menu styling.

--- src/view/home/skill/v_skill.php
+++ src/view/home/skill/v_skill.php
@@ -16,16 +16,16 @@ $skillTabId = $skillTab['id'];
 ?>
 <script src="/js/skill.js"></script>
 <div id="home-skill-content" class="rubrique clearfix">
-    <?php require_once VIEW . '/home/skill/v_skillTabWrapper.php' ?>
-    <nav aria-label="Page navigation example" class="mt-5 d-flex justify-content-center">
+    <nav aria-label="Page navigation example" class="mb-3 d-flex justify-content-center">
         <ul class="pagination" id='skillTabMenu'>
             <?php
-            foreach ($skillTabList as $key => $skillTab) {
-                $name = $skillTab['name'];
-                $id = $skillTab['id'];
+            foreach ($skillTabList as $key => $skillTabOfList) {
+                $name = $skillTabOfList['name'];
+                $id = $skillTabOfList['id'];
                 echo "<li class='page-item'><button class='page-link skillTabSelect " . ($name === $skillTabName ? 'active' : '') . "' type='button' value='$id'>$name</button></li>";
             }
             ?>
         </ul>
     </nav>
+    <?php require_once VIEW . '/home/skill/v_skillTabWrapper.php' ?>
 </div>
